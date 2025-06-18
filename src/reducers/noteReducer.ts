d0a3ea34ad53f9bc405a932df590f4814c94c4dd
LaraBerenguer <laraberenguer@outlook.com>
2025-06-18 20:20:42 +0200

    Enhance noteReducer to handle toggling importance

--- src/reducers/noteReducer.ts
+++ src/reducers/noteReducer.ts
@@ -4,5 +4,16 @@ export const noteReducer = (state: Note[] = [], action: { type: string; payload:
     if (action.type === "notes_created") {
         return state.concat(action.payload);
     }
-    return state
+
+    if (action.type === "notes_toggle_importance") {
+        const { id } = action.payload;
+        return state.map(note => {
+            if (note.id === id) {
+                return { ...note, important: !note.important }
+            }
+            return note;
+        });
+    };
+
+    return state;
 };
