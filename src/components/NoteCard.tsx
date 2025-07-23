6299433085c140b0933951d6b6169c28f41ce5e2
LaraBerenguer <laraberenguer@outlook.com>
2025-07-23 19:29:17 +0200

    Improve note card layout

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -9,7 +9,7 @@ type NoteCardProps = {
 
 const NoteCard = ({ note, onClick }: NoteCardProps) => {
     return (
-        <div className="note-card flex items-center border border-gray-300 p-4 rounded-lg bg-transparent cursor-pointer transition hover:shadow" onClick={onClick}>
+        <div className="note-card flex items-center border-2 border-gray-100 p-4 rounded-lg bg-transparent" onClick={onClick}>
             <section>
                 <section id="title" className="text-lg font-semibold mb-1">{note.title}</section>
                 <section id="content" className="text-sm text-gray-700">{note.content}</section>
