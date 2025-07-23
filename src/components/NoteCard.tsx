15afd3db87d57dea6e18e23e6982c70b5d9c4f9e
LaraBerenguer <laraberenguer@outlook.com>
2025-07-23 18:56:20 +0200

    Refactor NoteCard component to use ImportantIcon and uptate styles

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Note } from "@/types/types";
+import ImportantIcon from "./ImportantIcon";
 
 type NoteCardProps = {
     note: Note;
@@ -8,10 +9,12 @@ type NoteCardProps = {
 
 const NoteCard = ({ note, onClick }: NoteCardProps) => {
     return (
-        <div className="border border-gray-300 p-4 rounded-lg bg-transparent cursor-pointer transition hover:shadow" onClick={onClick}>
-            <section id="title" className="text-lg font-semibold mb-1">{note.title}</section>
-            <section id="content" className="text-sm text-gray-700">{note.content}</section>
-            <section id="important" className="text-xs font-bold text-red-500">{note.important ? " (important)" : ""}</section>
+        <div className="note-card flex items-center justify-between border border-gray-300 p-4 rounded-lg bg-transparent cursor-pointer transition hover:shadow" onClick={onClick}>
+            <section>
+                <section id="title" className="text-lg font-semibold mb-1">{note.title}</section>
+                <section id="content" className="text-sm text-gray-700">{note.content}</section>
+            </section>
+            <section id="important" className="text-xs font-bold text-red-500">{note.important && <ImportantIcon className="inline-block text-red-500 ml-2" />}</section>
         </div>
     )
 };
