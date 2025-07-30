e0cb98062a5dd93fbe26680f2e1b5f61d8c2da97
LaraBerenguer <laraberenguer@outlook.com>
2025-07-30 18:25:31 +0200

    Add delete functionality to NoteCard and integrate with NotesList

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -6,9 +6,10 @@ import { Trash } from "lucide-react";
 type NoteCardProps = {
     note: Note;
     onClick: () => void;
+    onDelete: (id: number) => void;
 }
 
-const NoteCard = ({ note, onClick }: NoteCardProps) => {
+const NoteCard = ({ note, onClick, onDelete }: NoteCardProps) => {    
     return (
         <div className="note-card flex items-center border-2 border-gray-100 p-4 rounded-lg bg-transparent" onClick={onClick}>
             <section>
@@ -16,7 +17,7 @@ const NoteCard = ({ note, onClick }: NoteCardProps) => {
                 <section id="content" className="text-sm text-gray-700">{note.content}</section>
             </section>
             <section id="important" className="text-xs font-bold text-red-500 ml-auto">{note.important && <ImportantIcon className="inline-block text-red-500 ml-2" />}</section>
-            <section id="delete-note"><Trash strokeWidth={1} size={18} /></section>
+            <section id="delete-note"><Trash strokeWidth={1} size={18} onClick={(e) => { e.stopPropagation(); onDelete(note.id) }} /></section>
         </div>
     )
 };
