aa6259b20e3a8c4e9df2e2f5a22d13bdeb0584bd
LaraBerenguer <laraberenguer@outlook.com>
2025-07-30 17:44:50 +0200

    Add custom hook for deleting notes

---
+++ src/hooks/useDeleteNote.ts
@@ -0,0 +1,26 @@
+"use client"
+import { notes_deleted } from "@/reducers/noteReducer";
+import { deleteNote } from "@/services/notesService";
+import { AppDispatch } from "@/store/store";
+import { Note } from "@/types/types";
+import { useDispatch } from "react-redux";
+
+const useDeleteNote = (id: Number) => {
+    const dispatch = useDispatch<AppDispatch>();
+    const deleteExistingNote = async (note: Note) => {
+        try {
+            const status = await deleteNote(id);
+            if (status === 200 || status === 204) {
+                dispatch(notes_deleted(id));
+                return { success: true, message: "Note deleted successfully" }
+            } else {
+                return { success: false, message: `Error deleting Note. Status code: ${status}` }
+            }
+        } catch (error) {
+            return { success: false, message: `Something went wrong (on our end)` }
+        }
+    };
+    return deleteExistingNote;
+};
+
+export default useDeleteNote;
