3286a0244a1e605f49b0918409ce529fc5773fca
LaraBerenguer <laraberenguer@outlook.com>
2025-08-25 16:02:18 +0200

    Add Enter key functionality to CreateNoteForm for submitting notes

--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -23,10 +23,10 @@ const CreateNoteForm = () => {
 
     return (
         <form className="flex flex-col gap-2 border-2 border-gray-100 p-4 rounded-lg bg-transparent" onSubmit={handleSubmit}>
-            <label className="create-note-inputs flex flex-col gap-2"> 
-                <input name="noteTitle" value={noteTitle} onChange={(e) => setNoteTitle(e.target.value)} placeholder="Title"></input>                
+            <label className="create-note-inputs flex flex-col gap-2" onKeyDown={(e) => e.key === "Enter" && handleSubmit}>
+                <input name="noteTitle" value={noteTitle} onChange={(e) => setNoteTitle(e.target.value)} placeholder="Title"></input>
                 <input name="noteContent" value={noteContent} onChange={(e) => setNoteContent(e.target.value)} placeholder="Hello one and all..."></input>
-            </label>            
+            </label>
             <button id="addNoteButton" className="px-3 py-1 rounded-md border-2 border-gray-100 bg-transparent hover:bg-gray-100 hover:cursor-pointer text-sm transition">Add</button>
         </form>
     );
