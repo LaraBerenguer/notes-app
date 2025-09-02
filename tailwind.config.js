d4c7a19e8dd0f03c60744f8d2247227c1f3d6bf5
LaraBerenguer <laraberenguer@outlook.com>
2025-09-02 16:16:56 +0200

    Clean css files of default nextjs variables. Add dark mode colors to tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                background: {
+                    light: "#ffffff",
+                    dark: "#202124",
+                },
+                foreground: {
+                    light: "#171717",
+                    dark: "#e8eaed",
+                },
+            },
+        },
+    },
     content: [
         "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
