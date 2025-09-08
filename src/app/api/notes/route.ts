e416f68790c416c1eecdb6cd69c1dd5dcad3a143
LaraBerenguer <laraberenguer@outlook.com>
2025-09-08 17:43:07 +0200

    Refactor Note types and improve type definitions for NewNote and Note models

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -5,12 +5,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
-    const notes = await prisma.note.findMany({});
+    const notes: Note[] = await prisma.note.findMany({});
     return NextResponse.json(notes);
 };
 
 export async function POST(request: Request) {
-    const newNote = await request.json();
-    const noteCreated = await prisma.note.create({ data:newNote });
+    const newNote: NewNote = await request.json();
+    //temporal while implementing users and auth
+    const { userId, ...noteData } = newNote;
+    const noteCreated: Note = await prisma.note.create({ data: userId === undefined ? noteData : newNote });
     return NextResponse.json(noteCreated, { status: 201 });
 };
