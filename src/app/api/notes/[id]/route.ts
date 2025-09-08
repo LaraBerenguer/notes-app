a6f7001a579d951cf59407bc736536aaa2761164
LaraBerenguer <laraberenguer@outlook.com>
2025-09-08 18:21:45 +0200

    Refactor DELETE method to use Prisma for database

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -5,14 +5,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function PATCH(request: Request, context: { params: { id: string } }) {
-    const {id: idParam} = await context.params;
+    const { id: idParam } = await context.params;
     const { title, content } = await request.json();
 
-    const editedNote = await prisma.note.update({
+    const editedNote: Note = await prisma.note.update({
         where: {
             id: Number(idParam),
         },
-
         data: {
             title,
             content,
@@ -24,13 +23,13 @@ export async function PATCH(request: Request, context: { params: { id: string } 
 
 export async function DELETE(request: Request, context: { params: { id: string } }) {
     const { id: idParam } = await context.params;
-    const id = Number(idParam);
-    const db = await readDB();
-    const index = db.notes.findIndex((note: Note) => note.id === id);
-    if (index === -1) return NextResponse.json({ error: "Note not found" }, { status: 404 });
 
-    const deleted = db.notes.splice(index, 1);
-    await writeDB(db);
+    const deletedNote: Note = await prisma.note.delete({
+        where: {
+            id: Number(idParam),
+        },
+    })
 
-    return NextResponse.json(deleted[0]);
+    if (!deletedNote) return NextResponse.json({ error: "Note not found" }, { status: 404 });
+    return NextResponse.json(deletedNote);
 }
