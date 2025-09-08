ecc3c4e327cfc899528654dc20023980cd65a59e
LaraBerenguer <laraberenguer@outlook.com>
2025-09-08 17:29:30 +0200

    Make userId optional in Note model. update migration. Refactor API to use Prisma for data handling

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,37 +1,16 @@
 import { NextResponse } from "next/server";
-import fs from "fs/promises";
-import path from "path";
-import { Note } from "@/types/types";
+import { NewNote, Note } from "@/types/types";
+import { PrismaClient } from "@prisma/client";
 
-//local path
-const dbPath = path.join(process.cwd(), "src", "data", "db.json");
-
-//read temporal db aka local json file
-const readDB = async () => {
-    const data = await fs.readFile(dbPath, "utf-8");
-    return JSON.parse(data);
-}
-
-const writeDB = async (data: Note[]) => {
-    await fs.writeFile(dbPath, JSON.stringify(data, null, 2), "utf-8");
-}
+const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
-    const dbData = await readDB();
-    return NextResponse.json(dbData.notes);
+    const notes = await prisma.note.findMany({});
+    return NextResponse.json(notes);
 };
 
 export async function POST(request: Request) {
     const newNote = await request.json();
-    const dbData = await readDB();
-
-    const dbNote = {
-        id: Date.now(),
-        ...newNote
-    };
-
-    dbData.notes.push(dbNote);
-    await writeDB(dbData);
-
-    return NextResponse.json(dbNote, { status: 201 });
+    const noteCreated = await prisma.note.create({ data:newNote });
+    return NextResponse.json(noteCreated, { status: 201 });
 };
