fd753ec019df48876e96c2a101c59958fa556136
LaraBerenguer <laraberenguer@outlook.com>
2025-09-09 14:40:39 +0200

    Refactor authentication setup: integrate Prisma adapter and update session callback

--- auth.ts
+++ auth.ts
@@ -1,8 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GitHub({
             clientId: process.env.GITHUB_ID!,
@@ -13,6 +18,14 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
+    callbacks: {
+        async session({session, user}: any) {
+            if (session?.user) {
+                session.user.id = user.id;
+            }
+            return session;
+        }
+    }
 };
 
 const handler = NextAuth(authOptions);
