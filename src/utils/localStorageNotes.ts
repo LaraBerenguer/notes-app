3f50f60319909d572d808135c45a4ac5af9b6c84
LaraBerenguer <laraberenguer@outlook.com>
2025-09-15 15:03:23 +0200

    Fix catch error and getGuestNotes argument

--- src/utils/localStorageNotes.ts
+++ src/utils/localStorageNotes.ts
@@ -3,17 +3,17 @@ import { NewNote } from "../types/types";
 export const saveGuestNotes = (notes: NewNote[]) => {
     try {
         localStorage.setItem("localStorageNotes", JSON.stringify(notes));
-    } catch {
+    } catch (error) {
         console.error("Something went wrong, sorry", error);
     }
 };
 
-export const getGuestNotes = (notes: Note[]) => {
+export const getGuestNotes = () => {
     try {
         const guestNotes = localStorage.getItem("localStorageNotes");
         if (!guestNotes) return [];
         return JSON.parse(guestNotes);
-    } catch {
+    } catch (error) {
         return [];
     }
 };
