e8fcf3565d4f1adc49fe7dd111ce918b34f411a0
LaraBerenguer <laraberenguer@outlook.com>
2025-09-16 12:36:15 +0200

    Add parseNote util to change notes structure for redux management. Refactor useNotesInit to use new util

--- src/hooks/useNotesInit.ts
+++ src/hooks/useNotesInit.ts
@@ -1,10 +1,11 @@
 "use client"
 import { notes_init } from "@/reducers/noteReducer";
-import { NewNote, Note, NoteForRedux } from "@/types/types";
+import { Note, NoteForRedux } from "@/types/types";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getGuestNotes } from "@/utils/localStorageNotes";
 import { useSession } from "next-auth/react";
+import { parseLocalNotes, parseNotesFromDb } from "@/utils/parseNotes";
 
 const useNotesInit = (notes: Note[]) => {
     const dispatch = useDispatch();
@@ -14,23 +15,10 @@ const useNotesInit = (notes: Note[]) => {
         if (!session) {
             //without user session from localStorage
             const localNotes = getGuestNotes();
-            notesForRedux = localNotes.map(note => ({
-                id: new Date().getTime(),
-                title: note.title,
-                content: note.content,
-                important: note.important ?? false,
-                color: note.color ?? "default",
-                createdAt: new Date().toISOString(),
-                updatedAt: new Date().toISOString(),
-                userId: null,
-            }));
+            notesForRedux = parseLocalNotes(localNotes);
         } else {
             //with user session from db
-            notesForRedux = notes.map(note => ({
-                ...note,
-                createdAt: typeof note.createdAt === "string" ? note.createdAt : note.createdAt.toISOString(),
-                updatedAt: typeof note.updatedAt === "string" ? note.updatedAt : note.updatedAt.toISOString(),
-            }));
+            notesForRedux = parseNotesFromDb(notes);
         };
         dispatch(notes_init(notesForRedux));
     }, [dispatch, notes, session]);
