f32a822d3d077b6a2765fcc394fd6985c770865d
LaraBerenguer <laraberenguer@outlook.com>
2025-09-16 11:59:11 +0200

    Refactor useNotesInit to utilize useSession for session management and fix conditional

--- src/hooks/useNotesInit.ts
+++ src/hooks/useNotesInit.ts
@@ -3,42 +3,37 @@ import { notes_init } from "@/reducers/noteReducer";
 import { NewNote, Note, NoteForRedux } from "@/types/types";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@auth";
 import { getGuestNotes } from "@/utils/localStorageNotes";
+import { useSession } from "next-auth/react";
 
 const useNotesInit = (notes: Note[]) => {
     const dispatch = useDispatch();
+    const { data: session } = useSession();
     useEffect(() => {
-        const init = async () => {
-            const session = await getServerSession(authOptions);
-            let notesForRedux: NoteForRedux[] = [];
-            if (session?.user) {
-                //with user session from db
-                notesForRedux = notes.map(note => ({
-                    ...note,
-                    createdAt: typeof note.createdAt === "string" ? note.createdAt : note.createdAt.toISOString(),
-                    updatedAt: typeof note.updatedAt === "string" ? note.updatedAt : note.updatedAt.toISOString(),
-                }));
-            } else {
-                //without user session from localStorage
-                const localNotes = getGuestNotes();
-                notesForRedux = localNotes.map(note => ({
-                    id: new Date().getTime(),
-                    title: note.title,
-                    content: note.content,
-                    important: note.important ?? false,
-                    color: note.color ?? "default",
-                    createdAt: new Date().toISOString(),
-                    updatedAt: new Date().toISOString(),
-                    userId: null,
-                }));
-            };
-            dispatch(notes_init(notesForRedux));
+        let notesForRedux: NoteForRedux[] = [];
+        if (!session) {
+            //without user session from localStorage
+            const localNotes = getGuestNotes();
+            notesForRedux = localNotes.map(note => ({
+                id: new Date().getTime(),
+                title: note.title,
+                content: note.content,
+                important: note.important ?? false,
+                color: note.color ?? "default",
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString(),
+                userId: null,
+            }));
+        } else {
+            //with user session from db
+            notesForRedux = notes.map(note => ({
+                ...note,
+                createdAt: typeof note.createdAt === "string" ? note.createdAt : note.createdAt.toISOString(),
+                updatedAt: typeof note.updatedAt === "string" ? note.updatedAt : note.updatedAt.toISOString(),
+            }));
         };
-
-        init();
-    }, [dispatch, notes]);
+        dispatch(notes_init(notesForRedux));
+    }, [dispatch, notes, session]);
 };
 
 export default useNotesInit;
