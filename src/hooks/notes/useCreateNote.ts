9ce34a77163fa7d174c7113af4492203164561fb
LaraBerenguer <laraberenguer@outlook.com>
2025-09-16 13:01:33 +0200

    Refactor useCreateNote to handle guest notes and update saveGuestNote to store a single note in localStorage as an Array

--- src/hooks/notes/useCreateNote.ts
+++ src/hooks/notes/useCreateNote.ts
@@ -3,15 +3,27 @@ import { notes_created } from "@/reducers/noteReducer";
 import { createNote } from "@/services/notesService";
 import { AppDispatch } from "@/store/store";
 import { NewNote, Note } from "@/types/types";
-import { parseNoteFromDb } from "@/utils/parseNotes";
+import { saveGuestNote } from "@/utils/localStorageNotes";
+import { parseLocalNote, parseNoteFromDb } from "@/utils/parseNotes";
+import { useSession } from "next-auth/react";
 import { useDispatch } from "react-redux";
 
 const useCreateNote = () => {
     const dispatch = useDispatch<AppDispatch>();
+    const { data: session } = useSession();
     const addNewNote = async (note: NewNote) => {
-        const noteCreated = await createNote(note);
-        const reduxNote = parseNoteFromDb(noteCreated)
-        dispatch(notes_created(reduxNote));
+        if (!session) {
+            //without user session from localStorage
+            //async?
+            saveGuestNote(note);
+            const reduxNote = parseLocalNote(note)
+            dispatch(notes_created(reduxNote));
+        } else {
+            //with user session from db
+            const noteCreated = await createNote(note);
+            const reduxNote = parseNoteFromDb(noteCreated)
+            dispatch(notes_created(reduxNote));
+        };
     };
     return addNewNote;
 };
--- src/utils/localStorageNotes.ts
+++ src/utils/localStorageNotes.ts
@@ -1,8 +1,8 @@
 import { NewNote } from "../types/types";
 
-export const saveGuestNotes = (notes: NewNote[]) => {
+export const saveGuestNote = (note: NewNote) => {
     try {
-        localStorage.setItem("localStorageNotes", JSON.stringify(notes));
+        localStorage.setItem("localStorageNotes", JSON.stringify(note));
     } catch (error) {
         console.error("Something went wrong, sorry", error);
     }
@@ -12,7 +12,8 @@ export const getGuestNotes = () => {
     try {
         const guestNotes = localStorage.getItem("localStorageNotes");
         if (!guestNotes) return [];
-        return JSON.parse(guestNotes) as NewNote[];
+        const parsed = JSON.parse(guestNotes);
+        return Array.isArray(parsed) ? parsed as NewNote[] : [parsed] as NewNote[]; //it always returns array for redux parsing
     } catch (error) {
         if (error instanceof Error) {
             console.error("Error loading notes, try again", error.message);
