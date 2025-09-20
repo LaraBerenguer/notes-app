7c68bb00a5523361aa2713376e7c25238014e3f7
LaraBerenguer <laraberenguer@outlook.com>
2025-09-20 12:07:32 +0200

    Change NoteEditForm.tsx button colors and vertical resize size

--- src/components/notes_forms/NoteEditForm.tsx
+++ src/components/notes_forms/NoteEditForm.tsx
@@ -29,14 +29,14 @@ const NoteEditForm = ({ note, setIsEditing, onEdit }: NoteEditFormProps) => {
                 aria-label="Edit note title"
             />
             <textarea
-                className="text-sm text-gray-700 h-32 resize-y focus:border-gray-100 active:border-gray-100 border-2 border-transparent rounded"
+                className="text-sm text-gray-700 h-18 resize-y focus:border-gray-100 active:border-gray-100 border-2 border-transparent rounded"
                 value={editContent}
                 onChange={e => setEditContent(e.target.value)}
                 placeholder={note.content}
                 aria-label="Edit note content"
             />
             <div className="note-card--buttons flex gap-2 mt-2">
-                <button className="px-2 py-1 bg-green-300 text-white rounded" onClick={saveEdit} aria-label="Save edit note">Save</button>
+                <button className="px-2 py-1 bg-pink-300 text-gray-800 rounded" onClick={saveEdit} aria-label="Save edit note">Save</button>
                 <button className="px-2 py-1 bg-gray-200 text-gray-800 rounded" onClick={() => setIsEditing(false)}>Cancel</button>
             </div>
         </div>
